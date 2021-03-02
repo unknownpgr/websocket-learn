f62fcc0114b3e81ffaa63dbe294c650de828ea64
unknownpgr <unknownpgr@gmail.com>
2021-03-02 22:45:12 +0000

    Add user list function

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const app = express();
 const server = http.Server(app);
 const io = ws(server);
 
+const users = {};
+
 app.use(express.static(join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
@@ -16,12 +18,24 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
+  users[socket.id] = 'NEW USER';
+
   socket.on('chat message', (msg) => {
     io.emit('chat reply', msg);
   });
 
   socket.on('new user', (msg) => {
+    users[socket.id] = msg;
     io.emit('chat reply', `New user ${msg} joined this chatroom.`);
+  });
+
+  socket.on('list', (msg) => {
+    socket.emit('list', JSON.stringify(Object.keys(users).map(x => users[x]).filter(x => x)));
+  });
+
+  socket.on('disconnect', () => {
+    io.emit('chat reply', 'User ' + users[socket.id] + " disconnected");
+    users[socket.id] = undefined;
   });
 });
 
