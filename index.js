94586171b840cf3b2956792ccc4a441f53bdb23a
unknownpgr <unknownpgr@gmail.com>
2021-03-03 00:08:44 +0000

    Add user list

--- index.js
+++ index.js
@@ -9,33 +9,74 @@ const app = express();
 const server = http.Server(app);
 const io = ws(server);
 
-const users = {};
+const users = [];
+function userAdd(newUser) {
+  if (!newUser.id) throw new Error("User must have a id");
+  if (users.filter(x => x.id == newUser.id).length > 0) throw new Error("User with same id already exists");
+  users.push(newUser);
+}
+
+function userRemove(userID) {
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].id == userID) {
+      users.splice(i, 1);
+      break;
+    }
+  }
+}
+
+function userFind(userID) {
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].id == userID) {
+      return users[i];
+    }
+  }
+  return null;
+}
 
 app.use(express.static(join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'public', 'index.html'));
+  res.send(join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/list', (req, res) => {
+  res.send(users);
 });
 
 io.on('connection', (socket) => {
-  users[socket.id] = 'NEW USER';
 
-  socket.on('chat message', (msg) => {
-    io.emit('chat reply', msg);
+  socket.on('chat', (msg) => {
+    let user = userFind(socket.id);
+    io.emit('chat', {
+      user: {
+        id: user.id,
+        name: user.name,
+        x: 0,
+        y: 0
+      }, msg: msg
+    });
   });
 
   socket.on('new user', (msg) => {
-    users[socket.id] = msg;
-    io.emit('chat reply', `New user ${msg} joined this chatroom.`);
-  });
-
-  socket.on('list', (msg) => {
-    socket.emit('list', JSON.stringify(Object.keys(users).map(x => users[x]).filter(x => x)));
+    userAdd({ id: socket.id, name: msg });
+    io.emit('notify', `User ${msg} connected.`);
+    io.emit('list', users);
   });
 
   socket.on('disconnect', () => {
-    io.emit('chat reply', 'User ' + users[socket.id] + " disconnected");
-    users[socket.id] = undefined;
+    let user = userFind(socket.id);
+    if (!user) return;
+    io.emit('notify', 'User ' + user.name + " disconnected");
+    userRemove(socket.id);
+  });
+
+  socket.on('move', data => {
+    let user = userFind(socket.id);
+    let { x, y } = data;
+    user.x = x;
+    user.y = y;
+    io.emit('move', user);
   });
 });
 
